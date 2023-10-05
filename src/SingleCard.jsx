adf751c14f3af456b6a9b90bcd49ed90a4b8081e
abishekpathak-bitkraft <abhishek.pathak@bitkraft.co.in>
2023-10-05 11:37:24 +0530

    add style to card

--- src/SingleCard.jsx
+++ src/SingleCard.jsx
@@ -2,26 +2,27 @@ import { Draggable } from 'react-beautiful-dnd';
 
 const SingleCard = ({ prod, index }) => {
 
-    const cardStyle = {
-        background: prod.done === true ? "yellow" : "red",
-        width: "300px",
-        textAlign: "center",
-        border: "2px solid black"
-    }
-
     return (
         <>
             <Draggable draggableId={prod.id.toString()} index={index}>
                 {
                     (provided) => (
                         <div
-                            style={cardStyle}
-                            
                             ref={provided.innerRef}
                             {...provided.dragHandleProps}
                             {...provided.draggableProps}
                         >
-                            <h2 ><span>{prod.id}: </span>{prod.title}</h2>
+                            <h2 style={{
+                                background: prod.done === true ? "yellow" : "red",
+                                width: "300px",
+                                textAlign: "center",
+                                border: "2px solid black",
+                                height:"50px",
+                                display:"flex",
+                                alignItems:"center",
+                                justifyContent:"center",
+                                borderRadius:"10px"
+                            }}><span>{prod.id}: </span>{prod.title}</h2>
                         </div>
                     )
                 }
